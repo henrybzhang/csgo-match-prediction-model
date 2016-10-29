c2ed04a589f93a8d27aa11cfaef7b584dae6b842
c0ver <henrybzhang.99@gmail.com>
2016-10-28 22:19:32 -0700

    reverted base.cpp back to normal

--- elosystems/base.cpp
+++ elosystems/base.cpp
@@ -1,4 +1,5 @@
 //basic skeleton of elo system
+//winning team not always first
 #include <fstream>
 #include <iostream>
 #include <algorithm>
@@ -13,15 +14,18 @@ int total_N_matches, total_N_players;
 int player_index[2][5];
 double team_average[2];
 
-bool enough_games, could_not_find_player;
+bool enough_games;
 
 int game_counter, N_correct_predictions;
 double mean_squared_error;
 
 struct match_stats
 {
-    int date, score[2];
+    int date, hour;
+    int score[2], player_kills[2][5], player_headshots[2][5], player_assists[2][5], player_deaths[2][5];
     string map_name, player_name[2][5], team_name[2];
+    double player_hltv_ratings[2][5];
+    bool team0_wins;
 };
 struct player_stats
 {
@@ -38,13 +42,22 @@ void input_data()
     fin >> total_N_matches;
     match = new match_stats[total_N_matches];
     for(int m=0; m<total_N_matches; m++){
-        fin >> match[m].date >> match[m].map_name;
+        fin >> match[m].date >> match[m].hour >> match[m].map_name;
 
-        fin >> match[m].team_name[0] >> match[m].score[0];
-        for(int y=0; y<5; y++) fin >> match[m].player_name[0][y];
-
-        fin >> match[m].team_name[1] >> match[m].score[1];
-        for(int y=0; y<5; y++) fin >> match[m].player_name[1][y];
+        for(int x=0; x<2; x++){
+            fin >> match[m].team_name[x] >> match[m].score[x];
+        }
+        if(match[m].score[0] > match[m].score[1]) match[m].team0_wins = true;
+        for(int x=0; x<2; x++){
+            for(int y=0; y<5; y++){
+                fin >> match[m].player_name[x][y];
+                fin >> match[m].player_kills[x][y];
+                fin >> match[m].player_headshots[x][y];
+                fin >> match[m].player_assists[x][y];
+                fin >> match[m].player_deaths[x][y];
+                fin >> match[m].player_hltv_ratings[x][y];
+            }
+        }
     }
     fin.close();
 
@@ -64,31 +77,29 @@ void reset_program()
     mean_squared_error = game_counter = N_correct_predictions = 0;
 }
 
-void find_player_positions(int match_number)
+bool find_player_index(int m)
 {
     enough_games = true;
-    could_not_find_player = false;
     for(int x=0; x<2; x++){
         for(int y=0; y<5; y++){
             for(int p=0; p<total_N_players; p++){
-                if(player[p].name == match[match_number].player_name[x][y]){
+                if(player[p].name == match[m].player_name[x][y]){
                     player_index[x][y] = p;
                     if(player[p].number_of_games < N_games_needed){
                         enough_games = false;
-                        return;
                     }
                     break;
                 }
                 if(p == total_N_players - 1){ // if player's name is not found in player name array
-                    cout << match[match_number].player_name[x][y] << endl;
-                    could_not_find_player = true;
-                    cout << "Error: could not find player '" << match[match_number].player_name[x][y] << "'" << endl;
-                    cout << "Match Number: " << match_number + 1 << endl;
-                    return;
+                    cout << match[m].player_name[x][y] << endl;
+                    cout << "Error: could not find player '" << match[m].player_name[x][y] << "'" << endl;
+                    cout << "Match Number: " << m + 1 << endl;
+                    return false;
                 }
             }
         }
     }
+    return true;
 }
 
 void find_team_average()
@@ -100,12 +111,12 @@ void find_team_average()
     }
 }
 
-void update_player_ratings(int match_number, double k)
+void update_player_ratings(int m, double elo_constant)
 {
     for(int x=0; x<2; x++){
         int counter = 1 - x;
 
-        double percentage_rounds_won = match[match_number].score[x] / (double)(match[match_number].score[0] + match[match_number].score[1]);
+        double percentage_rounds_won = match[m].score[x] / (double)(match[m].score[0] + match[m].score[1]);
         double base_score = percentage_rounds_won;
 
         // percentage * other team's rating average
@@ -116,7 +127,7 @@ void update_player_ratings(int match_number, double k)
             int p = player_index[x][y];
             player[p].number_of_games++;
             double expected_score = 1 / (1 + exp(team_average[counter] - player[p].rating));
-            player[p].rating += k * (base_score - expected_score);
+            player[p].rating += elo_constant * (base_score - expected_score);
         }
     }
 }
@@ -167,55 +178,69 @@ double cdf(double value)
    return 0.5 * erfc(-value * 1 / sqrt(2));
 }
 
-double test_program(int match_number)
+void swap(double &n1, double &n2)
+{
+     double temp = n1;
+     n1 = n2;
+     n2 = temp;
+}
+
+double test_program(int m)
 {
     double standard_deviation[2], mean[2];
     for(int x=0; x<2; x++){
         double team_player_ratings[5];
-        for(int y=0; y<5; y++) team_player_ratings[y] = player[player_index[x][y]].rating;
+        for(int y=0; y<5; y++){
+            int p = player_index[x][y];
+            team_player_ratings[y] = player[p].rating;
+        }
         mean[x] = get_mean(5, team_player_ratings);
         standard_deviation[x] = get_standard_deviation(5, mean[x], team_player_ratings);
     }
 
-    // the winning team is always the first team
-    if(team_average[0] > team_average[1]) N_correct_predictions++;
+    if(team_average[0] > team_average[1] && match[m].team0_wins == true) N_correct_predictions++;
+    else if(team_average[0] < team_average[1] && match[m].team0_wins == false) N_correct_predictions++;
     game_counter++;
+
+    //make mean[0] the winning team's mean rating
+    if(match[m].team0_wins == false){
+        swap(mean[0], mean[1]);
+        swap(standard_deviation[0], standard_deviation[1]);
+    }
 
     double zscore = (mean[0] - mean[1]) / sqrt((standard_deviation[0] * standard_deviation[0]) + (standard_deviation[1] * standard_deviation[1]));
     double probability = cdf(zscore);
     mean_squared_error += (1 - probability) * (1 - probability);
 }
 
-
 ofstream fout ("../results/base_predictions.txt");
-void output_tests(double k)
+void output_tests(double elo_constant)
 {
-    fout << "k: " << k << endl;
+    fout << "Elo Constant: " << elo_constant << endl;
     fout << "Predicted " << N_correct_predictions << " out of " << game_counter << " games correctly." << endl;
     fout << "Percentage: " << (double) N_correct_predictions / game_counter  * 100 << '%' << endl;
     fout << "Mean Squared Error: " << mean_squared_error / game_counter << endl << endl;
 }
-
 
 int main()
 {
     input_data();
 
     // try different constants to see which works best
-    for(int constant_1=1; constant_1<=1; constant_1++){
+    for(int constant_1=1; constant_1<=10; constant_1++){
         cout << "Constant_1: " << constant_1 << endl;
         reset_program();
-        double elo_constant = (double) constant1 / 10;
+        double elo_constant = (double) constant_1 / 10;
         for(int m=0; m<total_N_matches; m++){
-            find_player_positions(m);
-            if(could_not_find_player == true) continue;
+            if(find_player_index(m) == false) continue;
             find_team_average();
 
             //test the program here
             if(enough_games == true) test_program(m);
+
             update_player_ratings(m, elo_constant);
         }
         output_tests(elo_constant);
-        if(constant_1 == 1) output_data();
+        //output_data();
     }
 }
